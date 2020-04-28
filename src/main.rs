216992c5cc61a956412b6ed046cdce8fa801f349
wangxioqiang <wangxiaoqiang@hexindai.com>
2020-04-28 10:06:35 +0800

    xx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop{
+        println!("please input you guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess);
+    }
 }
